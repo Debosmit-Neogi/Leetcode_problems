327cf96646c8dba2f34a9f42167172d0b7537056
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2025-03-11 13:48:56 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (14.17%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+    
+        # base cases
+
+        if (n==0):
+            return 1
+        if(n==1):
+            return 1
+        
+        prev1, prev2 = 1,1
+        for i in range(2, n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
