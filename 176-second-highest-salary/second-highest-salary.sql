d74221e4aab4101db1d2724f7cfbda456d58260a
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2025-09-15 00:01:08 +0530

    Time: 255 ms (83.24%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,9 +1,1 @@
-# Write your MySQL query statement below
-# CASE WHEN COUNT(1) > 0 THEN 1 ELSE 0 END AS [Value]
-
-SELECT 
-  MAX(Salary) AS SecondHighestSalary
-FROM 
-  Employee
-WHERE 
-  Salary < (SELECT MAX(Salary) FROM Employee);
+SELECT MAX(salary) as SecondHighestSalary  from Employee where salary < (SELECT MAX(salary) from Employee)
