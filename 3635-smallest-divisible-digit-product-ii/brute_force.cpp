c44a0cfac62d8d5bd0068c3e697bebbf9f4adbaf
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2026-08-07 16:28:25 +0530

    Update brute_force.cpp

--- 3635-smallest-divisible-digit-product-ii/brute_force.cpp
+++ 3635-smallest-divisible-digit-product-ii/brute_force.cpp
@@ -1,1 +1,39 @@
 // Brute force solution
+class Solution {
+public:
+
+    long long int product_nums(int x){
+        long long int ans = 1;
+
+        while(x){
+            ans = ans*(x%10);
+            x = x/10;
+        }
+
+        return ans;
+    }
+
+    bool zero_free(int x){
+        while(x){
+            int digit = x%10;
+
+            if(digit == 0){
+                return false;
+            }
+
+            x = x/10;
+        }
+        return true;
+    }
+    string smallestNumber(string num, long long t) {
+
+        long long int num_int = stoi(num);
+        long long int ans = num_int;
+        while (product_nums(ans) % t != 0 || !zero_free(ans)){
+            ans = ans+1;
+        }
+        string ans_str = to_string(ans);
+        return ans_str;
+        
+    }
+};
