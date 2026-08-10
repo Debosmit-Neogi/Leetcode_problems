2964acdeb079397c5008b5a4805eaed7fb67fe6e
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2026-08-10 15:51:22 +0530

    Time: 29 ms (83.73%) | Memory: 9 MB (73.64%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
+
+        // dp[i] = true if the current player
+        // can force a win with i stones.
         vector<bool> dp(n + 1, false);
 
         // dp[0] = false
-        // If there are 0 stones, current player cannot move,
-        // so current player loses.
+        // No stones means the current player
+        // cannot make a move.
 
         for (int i = 1; i <= n; i++) {
 
+            // Try every perfect square <= i
             for (int x = 1; x * x <= i; x++) {
 
-                int remaining = i - x * x;
+                int square = x * x;
 
-                // If opponent is in a losing state,
-                // current player can win.
+                // State after making this move
+                int remaining = i - square;
+
+                // If the opponent is in a losing state,
+                // then this is a winning move.
                 if (dp[remaining] == false) {
+
                     dp[i] = true;
+
+                    // One winning move is enough.
                     break;
                 }
             }
