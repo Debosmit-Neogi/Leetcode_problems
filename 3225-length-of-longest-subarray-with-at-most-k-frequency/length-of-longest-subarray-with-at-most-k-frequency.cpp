3e6e7e6552d502773ff714e9258eb1bcae560b89
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2026-08-12 15:54:28 +0530

    Time: 59 ms (78.88%) | Memory: 149.4 MB (27.87%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+
+        // Stores the frequency of each number
+        // inside the current sliding window.
         unordered_map<int, int> freq;
+
+        // Left boundary of our sliding window
         int left = 0;
+
+        // Stores the maximum length found so far
         int ans = 0;
 
+        // Expand the window by moving right
         for (int right = 0; right < nums.size(); right++) {
+
+            // Add the current element to the window
             freq[nums[right]]++;
 
+            // If the frequency of nums[right] becomes
+            // greater than k, the window is invalid.
+            //
+            // Keep removing elements from the left
+            // until the window becomes valid again.
             while (freq[nums[right]] > k) {
+
+                // Remove nums[left] from the window
                 freq[nums[left]]--;
+
+                // Move the left boundary forward
                 left++;
             }
 
+            // At this point, the window [left ... right]
+            // is guaranteed to be a good subarray.
+            //
+            // Length of the current window:
+            // right - left + 1
             ans = max(ans, right - left + 1);
         }
 
+        // Return the longest good subarray length
         return ans;
     }
 };
